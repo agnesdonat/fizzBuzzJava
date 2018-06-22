c31b5229b41eb3a56fab6b63bd6933d4ea04179f
Noel Vock <noel1uk@gmail.com>
2018-06-22 17:23:20 +0100

    initial commit

---
+++ src/project3/fizzbuzz.java
@@ -0,0 +1,13 @@
+package project3;
+
+public class fizzbuzz {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int convert(int number) {
+		return 1;
+	}
+
+}
---
+++ src/project3/fizzbuzzTest.java
@@ -0,0 +1,14 @@
+package project3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class fizzbuzzTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, fizzbuzz.convert(1));
+	}
+
+}
